de07415d4da79a1342242894ba194c031503e562
pinp <pinp_zh@163.com>
2019-07-05 20:38:20 +0800

    Update trigger_time_delay.py

--- follow_up/trigger_time_delay.py
+++ follow_up/trigger_time_delay.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os,sys,time,datetime
+import time,datetime
 
 def trigger_type_of_time(trigger_time):
+    ### Suppose input using utc time.
     time_now = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
     trigger_time_t = time.mktime(time.strptime(trigger_time, '%Y-%m-%d %H:%M:%S'))
     time_now_t = time.mktime(time.strptime(time_now, '%Y-%m-%d %H:%M:%S'))
-    if (time_now_t - trigger_time_t) <= 180:
+    if (time_now_t - trigger_time_t) <= 180: ### 3 mins
         mark4 = 10
-    elif (time_now_t - trigger_time_t) <= 1800:
+    elif 180 < (time_now_t - trigger_time_t) <= 1800: ### 30 mins
         mark4 = 20
-    else:
+    elif (time_now_t - trigger_time_t) > 1800:
         mark4 = 30
+    else:
+        print 'Time delay check Failed'
+        mark4 = 0
     return mark4
 
 def trigger_type_of_init(alert_message_type):
@@ -21,6 +25,6 @@ def trigger_type_of_init(alert_message_type):
     elif alert_message_type == 'Update':
         mark2 = 2000
     else:
-        mark2 = 0
         print "Something is Wrong."
+        mark2 = 0
     return mark2
